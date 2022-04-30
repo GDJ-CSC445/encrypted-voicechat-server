345b2792038f1936351294db99e41c6f8574b75d
Gregory Maldonado <gmaldona@oswego.edu>
2022-04-30 19:04:04 -0400

    updated start_server scripts

---
+++ start_server-1.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Shell script for cs server easy deployment
+# Problem on the Oswego servers where the version of java does not work with maven
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+source etc/environment
+
+mvn clean
+mvn package
+mvn assembly:single
+clear
+java -jar target/encrypted-voicechat-server-1.0-SNAPSHOT-jar-with-dependencies.jar # edu.oswego.cs.VoicechatServer
